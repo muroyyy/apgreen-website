03f9432c19667ae49120350d5e6bae0ac815033d
muroyyy <amirul.faiz01@gmail.com>
2025-07-20 14:56:58 +0800

    update vite.config.ts and package.json for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/apgreen-website/', // <-- required for GitHub Pages
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
